0f1d763c6b8da4d53d6c4c51ea7ab19713cce4d0
Sourabh <sourabh@unolo.com>
2022-04-12 23:03:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myservicetutorial/services/MyBackgroundService.kt
@@ -0,0 +1,36 @@
+package com.example.myservicetutorial.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyBackgroundService : Service() {
+
+    private val TAG = "MyBackgroundService"
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        Thread {
+            while (true) {
+                Log.e(TAG, "Service is running...")
+                try {
+                    Thread.sleep(2000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }.start()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+
+
+
+    override fun onBind(p0: Intent?): IBinder? {
+     return null;
+    }
+
+
+}
